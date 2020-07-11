a9e6651f53f3d1b72e0578a2cb4fbc7f407c0dc8
kangbrilliant <kangb93@126.com>
2020-07-11 19:18:50 +0800

    Update data_pro.py

--- data/data_pro.py
+++ data/data_pro.py
@@ -16,8 +16,6 @@ def read_data(path):
             if slot_label not in slot_list:
                 slot_list.append(slot_label)
         if len(line)==1 and line!=[""]:
-
-
             intent = line[0]
             if intent == "atis_airfare#atis_flight":
                 print(sen_nums)
@@ -28,6 +26,8 @@ def read_data(path):
                 intent_list.append(intent)
             sen_nums = sen_nums+1
             slot_labels = []
+
+
 if __name__ == '__main__':
     path = "./snips/"
     read_data(path+"train.txt")
@@ -39,23 +39,3 @@ if __name__ == '__main__':
         fw.write(i+"\n")
     fw.close()
     print(intent_slot_list)
-    # vocab_list = sorted([_ for _ in vocab.items() if _[1] >= 1], key=lambda x: x[1], reverse=True)
-    #
-    # vocab_list = [word_count[0] for word_count in vocab_list]
-    # vocab_list = ["<pad>"]+["<unk>"]+["</s>"]+["</e>"]+vocab_list
-    # fw = open(path + "vocab.txt","w",encoding="utf-8")
-    # for line in vocab_list:
-    #     fw.write(line+"\n")
-    # fw.close()
-    # intent_list.sort()
-    #
-    # slot_list = sorted([slot for slot in slot_list if len(slot) >1 or slot=="O"], key=lambda x: x)
-    # slot_list = ["<PAD>"]+["<start>"]+["<end>"] + slot_list
-    # f_intent = open(path + "intent_label.txt","w", encoding="utf-8")
-    # f_slot = open(path+"slot_label.txt","w",encoding="utf-8")
-    # for idx,line in enumerate(intent_list):
-    #     f_intent.write(str(idx)+"\t"+line+"\n")
-    # f_intent.close()
-    # for idx,line in enumerate (slot_list):
-    #     f_slot.write(str(idx)+"\t"+line+"\n")
-    # f_slot.close()
